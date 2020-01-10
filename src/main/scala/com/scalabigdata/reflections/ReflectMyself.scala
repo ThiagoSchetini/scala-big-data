efff083aac2102482380bc1b0526b4cb84dbc6df
thiagoschetini <thiagoschetini@gmail.com>
2020-01-10 00:03:01 -0200

    adding auto serialize to CSV

--- src/main/scala/com/scalabigdata/reflections/ReflectMyself.scala
+++ src/main/scala/com/scalabigdata/reflections/ReflectMyself.scala
@@ -1,5 +1,8 @@
 package com.scalabigdata.reflections
 
+import org.apache.hadoop.fs.Path
+
+import scala.collection.mutable
 import scala.reflect.runtime.universe._
 import scala.util.control.Breaks._
 
@@ -11,6 +14,8 @@ object Executor extends App {
   val obj = ReflectMyselfProps(name, props)
   val result1 = obj.validateMe
   println(s"test1 validation result: $result1")
+
+  println(obj.toString)
 
 
   /* test 2 using try/catch to creation */
@@ -45,6 +50,10 @@ object Executor extends App {
   val result5 = obj5.validateMe
   println(s"test5 validation result: $result5")
 
+
+  println(s"\nSerializedCSV:")
+  println(s"${obj.serializeToCSV}")
+
 }
 
 trait ReflectMyself {
@@ -67,12 +76,12 @@ trait ReflectMyself {
         value match {
           case v: List[Any] =>
             if (v.nonEmpty)
-              v.foreach(i =>
+              v.foreach(i => {
                 if (i.asInstanceOf[String].isEmpty) {
                   flag = false
                   break
                 }
-              )
+              })
 
           case _ =>
             if (value == null || value.toString.isEmpty) {
@@ -84,6 +93,44 @@ trait ReflectMyself {
     }
 
     flag
+  }
+
+  private def serializeList(list: List[String]): String = {
+    val builder = new StringBuilder()
+    val last = list.last
+    val noLast = list.reverse.tail.reverse
+    noLast.foreach(i => builder.append(s"$i,"))
+    builder.append(last)
+    builder.mkString
+  }
+
+  def serializeToCSV: StringBuilder = {
+    val rm = scala.reflect.runtime.currentMirror
+    val instanceMirror = rm.reflect(this)
+    val accessors = rm.classSymbol(this.getClass).toType.members.collect {
+      case m: MethodSymbol if m.isGetter && m.isPublic => m
+    }
+    val orderedFields = this.getClass.getDeclaredFields.toList.map(_.getName)
+    val unorderedFields = new mutable.HashMap[String, String]()
+
+    /* get unordered values */
+    for (a <- accessors) {
+      val value = instanceMirror.reflectMethod(a).apply()
+
+      value match {
+        case v: List[Any] => unorderedFields += a.name.decodedName.toString -> serializeList(v.asInstanceOf[List[String]])
+        case v: Any => unorderedFields += a.name.decodedName.toString -> v.toString
+      }
+    }
+
+    /* using original class sequence order */
+    val fields = orderedFields zip orderedFields.map(unorderedFields)
+
+    /* delimited out with only values */
+    val csv = new StringBuilder()
+    val delimiter = "|"
+    fields.foreach(f => csv.append(s"${f._2}$delimiter"))
+    csv.dropRight(1)
   }
 }
 
